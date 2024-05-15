aff04a03218fd9e3804398a11c04ba7e3ba4eb4a
manvir singh <manvirsinghraul@gmail.com>
2024-05-15 22:57:08 +0530

    second commit

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,11 +1,18 @@
+'use client';
+import '@/app/ui/global.css';
+import { inter } from '@/app/ui/fonts';
+import { usePathname } from 'next/navigation';
+
 export default function RootLayout({
   children,
 }: {
   children: React.ReactNode;
 }) {
+  const pathname = usePathname();
+  console.log('pathname', pathname);
   return (
     <html lang="en">
-      <body>{children}</body>
+      <body className={`${inter.className} antialiased`}>{children}</body>
     </html>
   );
 }
